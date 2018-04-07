65ce718a89d3c6a9a9e23e5f67a1bb503398be72
Cole Taylor <coletaylor788@gmail.com>
2018-04-07 17:12:08 -0400

    Changing from local mode

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ Main method starts the Flask server
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
 
     #TESTING ONLY! Leave commented in production
-    app.run(host='127.0.0.1', port=port)
+    #app.run(host='127.0.0.1', port=port)
